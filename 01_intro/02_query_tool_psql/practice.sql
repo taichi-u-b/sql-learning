d98896c6bd0c0354a246cb977398abdd9ad82321
taichi-u-b <hanshin97@icloud.com>
2026-04-27 13:33:56 +0900

    Update practice.sql

--- 01_intro/02_query_tool_psql/practice.sql
+++ 01_intro/02_query_tool_psql/practice.sql
@@ -1,1 +1,27 @@
+# practice.sql
 
+```sql
+-- 入門編 第2章 Query Tool / psql に慣れる
+-- 演習記録：books テーブルを使った基本操作
+
+-- すでに books テーブルがある場合は削除する
+DROP TABLE IF EXISTS books;
+
+-- books テーブルを作成する
+CREATE TABLE books (
+    id INTEGER,
+    title TEXT,
+    price INTEGER
+);
+
+-- books テーブルにデータを追加する
+INSERT INTO books VALUES (1, 'SQL入門', 1800);
+INSERT INTO books VALUES (2, 'データベース基礎', 2200);
+INSERT INTO books VALUES (3, 'PostgreSQL練習帳', 2500);
+
+-- books テーブルのすべてのデータを確認する
+SELECT * FROM books;
+
+-- title と price だけを表示する
+SELECT title, price
+FROM books;
